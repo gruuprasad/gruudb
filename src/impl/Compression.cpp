6881fa7505bfd0b8c3331b68dd2e6e07894e9942
guruhegde <guruhegde1308@gmail.com>
2018-01-08 21:58:41 +0100

    refactor code for task 3.1

--- src/impl/Compression.cpp
+++ src/impl/Compression.cpp
@@ -1,5 +1,6 @@
 #include "impl/Compression.hpp"
 #include "impl/ColumnStore.hpp"
+#include "dbms/Store.hpp"
 
 
 using namespace dbms;
@@ -7,139 +8,60 @@ using namespace dbms;
 
 ColumnStore * dbms::compress_columnstore_lineitem(const Relation &relation, const ColumnStore &store)
 {
-    std::vector<ColumnBase*> compressed_columns;
+    Column<RLE<uint8_t>> * col0 = new Column<RLE<uint8_t>>();
+    Column<RLE<uint64_t>> * col1 = new Column<RLE<uint64_t>>();
+    Column<RLE<uint8_t>> * col2 = new Column<RLE<uint8_t>>();
+    Column<RLE<uint64_t>> * col3 = new Column<RLE<uint64_t>>();
+    Column<RLE<uint32_t>> * col4 = new Column<RLE<uint32_t>>();
+    Column<RLE<uint64_t>> * col5 = new Column<RLE<uint64_t>>();
+    Column<RLE<Char<26>>> * col6 = new Column<RLE<Char<26>>>();
+    Column<RLE<uint32_t>> * col7 = new Column<RLE<uint32_t>>();
+    Column<RLE<uint32_t>> * col8 = new Column<RLE<uint32_t>>();
+    Column<RLE<uint32_t>> * col9 = new Column<RLE<uint32_t>>();
+    Column<RLE<uint32_t>> * col10 = new Column<RLE<uint32_t>>();
+    Column<RLE<uint32_t>> * col11 = new Column<RLE<uint32_t>>();
+    Column<RLE<uint32_t>> * col12 = new Column<RLE<uint32_t>>();
+    Column<RLE<Char<11>>> * col13 = new Column<RLE<Char<11>>>();
+    Column<RLE<Char<45>>> * col14 = new Column<RLE<Char<45>>>();
+    Column<RLE<uint64_t>> * col15 = new Column<RLE<uint64_t>>();
 
-
-    //access uncompressed store column 1
-    Column<RLE<char>> * col1 = new Column<RLE<char>>();
-    auto orig_col1_it = store.get_column<char>(0).begin();
-
-    for (unsigned i = 0; i < store.size(); ++i) {
-        col1->push_back(*orig_col1_it);
-        ++orig_col1_it;
-    }
-
-    //access uncompressed store column 2
-    Column<RLE<int64_t>> * col2 = new Column<RLE<int64_t>>();
-    auto orig_col2_it = store.get_column<int64_t>(1).begin();
-
-    for (unsigned i = 0; i < store.size(); ++i) {
-        col2->push_back(*orig_col2_it);
-        ++orig_col2_it;
-    }
-    //access uncompressed store column 3
-    Column<RLE<char>> * col3 = new Column<RLE<char>>();
-    auto orig_col3_it = store.get_column<char>(2).begin();
-
-    for (unsigned i = 0; i < store.size(); ++i) {
-        col3->push_back(*orig_col3_it);
-        ++orig_col3_it;
-    }
-    //access uncompressed store column 4
-    Column<RLE<int64_t>> * col4 = new Column<RLE<int64_t>>();
-    auto orig_col4_it = store.get_column<int64_t>(3).begin();
-
-    for (unsigned i = 0; i < store.size(); ++i) {
-        col4->push_back(*orig_col4_it);
-        ++orig_col4_it;
-    }
-    //access uncompressed store column 5
-    Column<RLE<int32_t>> * col5 = new Column<RLE<int32_t>>();
-    auto orig_col5_it = store.get_column<int32_t>(4).begin();
+    auto orig_col0_it = store.get_column<uint8_t>(0).begin();
+    auto orig_col1_it = store.get_column<uint64_t>(1).begin();
+    auto orig_col2_it = store.get_column<uint8_t>(2).begin();
+    auto orig_col3_it = store.get_column<uint64_t>(3).begin();
+    auto orig_col4_it = store.get_column<uint32_t>(4).begin();
+    auto orig_col5_it = store.get_column<uint64_t>(5).begin();
+    auto orig_col6_it = store.get_column<Char<26>>(6).begin();
+    auto orig_col7_it = store.get_column<uint32_t>(7).begin();
+    auto orig_col8_it = store.get_column<uint32_t>(8).begin();
+    auto orig_col9_it = store.get_column<uint32_t>(9).begin();
+    auto orig_col10_it = store.get_column<uint32_t>(10).begin();
+    auto orig_col11_it = store.get_column<uint32_t>(11).begin();
+    auto orig_col12_it = store.get_column<uint32_t>(12).begin();
+    auto orig_col13_it = store.get_column<Char<11>>(13).begin();
+    auto orig_col14_it = store.get_column<Char<45>>(14).begin();
+    auto orig_col15_it = store.get_column<uint64_t>(15).begin();
 
     for (unsigned i = 0; i < store.size(); ++i) {
-        col5->push_back(*orig_col5_it);
-        ++orig_col5_it;
+        col0->push_back(*orig_col0_it++);
+        col1->push_back(*orig_col1_it++);
+        col2->push_back(*orig_col2_it++);
+        col3->push_back(*orig_col3_it++);
+        col4->push_back(*orig_col4_it++);
+        col5->push_back(*orig_col5_it++);
+        col6->push_back(*orig_col6_it++);
+        col7->push_back(*orig_col7_it++);
+        col8->push_back(*orig_col8_it++);
+        col9->push_back(*orig_col9_it++);
+        col10->push_back(*orig_col10_it++);
+        col11->push_back(*orig_col11_it++);
+        col12->push_back(*orig_col12_it++);
+        col13->push_back(*orig_col13_it++);
+        col14->push_back(*orig_col14_it++);
+        col15->push_back(*orig_col15_it++);
     }
-    //access uncompressed store column 6
-    Column<RLE<char>> * col6 = new Column<RLE<char>>();
-    auto orig_col6_it = store.get_column<char>(5).begin();
 
-    for (unsigned i = 0; i < store.size(); ++i) {
-        col6->push_back(*orig_col6_it);
-        ++orig_col6_it;
-    }
-    //access uncompressed store column 5
-    Column<RLE<int32_t>> * col7 = new Column<RLE<int32_t>>();
-    auto orig_col7_it = store.get_column<int32_t>(6).begin();
-
-    for (unsigned i = 0; i < store.size(); ++i) {
-        col7->push_back(*orig_col7_it);
-        ++orig_col7_it;
-    }
-    //access uncompressed store column 5
-    Column<RLE<int32_t>> * col8 = new Column<RLE<int32_t>>();
-    auto orig_col8_it = store.get_column<int32_t>(7).begin();
-
-    for (unsigned i = 0; i < store.size(); ++i) {
-        col8->push_back(*orig_col8_it);
-        ++orig_col8_it;
-    }
-    //access uncompressed store column 5
-    Column<RLE<int32_t>> * col9 = new Column<RLE<int32_t>>();
-    auto orig_col9_it = store.get_column<int32_t>(8).begin();
-
-    for (unsigned i = 0; i < store.size(); ++i) {
-        col9->push_back(*orig_col9_it);
-        ++orig_col9_it;
-    }
-    //access uncompressed store column 5
-    Column<RLE<int32_t>> * col10 = new Column<RLE<int32_t>>();
-    auto orig_col10_it = store.get_column<int32_t>(9).begin();
-
-    for (unsigned i = 0; i < store.size(); ++i) {
-        col10->push_back(*orig_col10_it);
-        ++orig_col10_it;
-    }
-    //access uncompressed store column 5
-    Column<RLE<int32_t>> * col11 = new Column<RLE<int32_t>>();
-    auto orig_col11_it = store.get_column<int32_t>(10).begin();
-
-    for (unsigned i = 0; i < store.size(); ++i) {
-        col11->push_back(*orig_col11_it);
-        ++orig_col11_it;
-    }
-    //access uncompressed store column 5
-    Column<RLE<int32_t>> * col12 = new Column<RLE<int32_t>>();
-    auto orig_col12_it = store.get_column<int32_t>(11).begin();
-
-    for (unsigned i = 0; i < store.size(); ++i) {
-        col12->push_back(*orig_col12_it);
-        ++orig_col12_it;
-    }
-    //access uncompressed store column 5
-    Column<RLE<int32_t>> * col13 = new Column<RLE<int32_t>>();
-    auto orig_col13_it = store.get_column<int32_t>(12).begin();
-
-    for (unsigned i = 0; i < store.size(); ++i) {
-        col13->push_back(*orig_col13_it);
-        ++orig_col13_it;
-    }
-    //access uncompressed store column 5
-    Column<RLE<char>> * col14 = new Column<RLE<char>>();
-    auto orig_col14_it = store.get_column<char>(13).begin();
-
-    for (unsigned i = 0; i < store.size(); ++i) {
-        col14->push_back(*orig_col14_it);
-        ++orig_col14_it;
-    }
-    //access uncompressed store column 5
-    Column<RLE<char>> * col15 = new Column<RLE<char>>();
-    auto orig_col15_it = store.get_column<char>(14).begin();
-
-    for (unsigned i = 0; i < store.size(); ++i) {
-        col15->push_back(*orig_col15_it);
-        ++orig_col15_it;
-    }
-    //access uncompressed store column 5
-    Column<RLE<int64_t>> * col16 = new Column<RLE<int64_t>>();
-    auto orig_col16_it = store.get_column<int64_t>(15).begin();
-
-    for (unsigned i = 0; i < store.size(); ++i) {
-        col16->push_back(*orig_col16_it);
-        ++orig_col16_it;
-    }
-    ColumnStore* compress_colstore = new ColumnStore(ColumnStore::Create_Explicit({col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15, col16}));
+    ColumnStore* compress_colstore = new ColumnStore(ColumnStore::Create_Explicit({col0, col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15}));
 
     return compress_colstore;
 }
